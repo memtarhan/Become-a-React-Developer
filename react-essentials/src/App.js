c9c12d5afc0c6ee5409fadc34ac48cedbd0c89e3
Mehmet Tarhan <hi@memtarhan.com>
2022-01-21 22:59:09 +0000

    [React Essentials] - Displaying data from an API

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -12,9 +12,24 @@ function App() {
 
   console.log(data);
   if (data) {
-    return <div>{JSON.stringify(data)}</div>;
+    return (
+      <div>
+        <h1>{data.count}</h1>
+        <ul>
+          {data.results.map((result) => (
+            <li>
+              <div>
+                <h2>{result.ticker}</h2>
+                <h3>{result.description}</h3>
+                <h4>{result.geography}</h4>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
   }
-  return <div>No user available</div>;
+  return <div>No data available</div>;
 }
 
 export default App;
