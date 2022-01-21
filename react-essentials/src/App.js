e89b2b70b5e456832ae4fd1a849819d32db5b5f8
Mehmet Tarhan <hi@memtarhan.com>
2022-01-21 23:11:50 +0000

    [React Essentials] - Handling loading states

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -3,32 +3,42 @@ import React, { useState, useEffect } from "react";
 
 function App() {
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
+    setLoading(true);
     fetch(`https://www.econdb.com/api/series/?format=json`)
       .then((response) => response.json())
-      .then(setData);
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError);
   }, []);
 
-  console.log(data);
-  if (data) {
-    return (
-      <div>
-        <h1>{data.count}</h1>
-        <ul>
-          {data.results.map((result) => (
-            <li>
-              <div>
-                <h2>{result.ticker}</h2>
-                <h3>{result.description}</h3>
-                <h4>{result.geography}</h4>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
+  if (loading) return <h1>Loading...</h1>;
+  if (error) {
+    return <pre>{JSON.stringify(error, null, 2)}</pre>;
   }
+  if (!data) {
+    return null;
+  }
+
+  return (
+    <div>
+      <ul>
+        {data.results.map((result) => (
+          <li>
+            <div>
+              <h2>{result.ticker}</h2>
+              <h3>{result.description}</h3>
+              <h4>{result.geography}</h4>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+  
   return <div>No data available</div>;
 }
 
