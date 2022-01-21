964dbbff55b53e09ded1cddd80b35249a87770e1
Mehmet Tarhan <hi@memtarhan.com>
2022-01-21 21:16:58 +0000

    [React Essentials] - Understanding the useState Hook

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -1,23 +1,26 @@
 import './App.css';
+import React, { useState } from "react";
 
 function SecretComponent() {
-  return (
-    <h1>Super secret information for authorized users only</h1>
-  )
+  return <h1>Super secret information for authorized users only</h1>;
 }
 
 function RegularComponent() {
-  return (
-    <h1>Just some public information</h1>
-  )
+  return <h1>Just some public information</h1>;
 }
 
-function App(props) {
+function App() {
+  const [emotion, setEmotion] = useState("happy");
+  console.log(emotion);
+
   return (
     <>
-      {props.authorized ? <SecretComponent/> : <RegularComponent/>}
+      <h1>Current emotion is {emotion}</h1>
+      <button onClick={() => setEmotion("happy")}>Happy</button>
+      <button onClick={() => setEmotion("frustrated")}>Frustrate</button>
+      <button onClick={() => setEmotion("enthusiastic")}>Enthuse</button>
     </>
-  )
+  );
 }
 
 export default App;
--- react-essentials/src/index.js
+++ react-essentials/src/index.js
@@ -3,7 +3,4 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from "./App"
 
-ReactDOM.render(
-  <App authorized={false}/>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
