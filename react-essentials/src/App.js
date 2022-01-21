1a305114824bf9358bf60f8c72f05c8fd1514bdc
Mehmet Tarhan <hi@memtarhan.com>
2022-01-21 21:25:31 +0000

    [React Essentials] - Working with useEffect

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -1,22 +1,24 @@
 import './App.css';
-import React, { useState } from "react";
-
-function SecretComponent() {
-  return <h1>Super secret information for authorized users only</h1>;
-}
-
-function RegularComponent() {
-  return <h1>Just some public information</h1>;
-}
+import React, { useState, useEffect } from "react";
 
 function App() {
   const [emotion, setEmotion] = useState("happy");
-  console.log(emotion);
+  const [secondary, setSecondary] = useState("tired");
+
+  useEffect(() => {
+    console.log(`It's ${emotion} around here`);
+  }, [emotion]);
+
+  useEffect(() => {
+    console.log(`It's ${secondary} around here`);
+  }, [secondary]);
 
   return (
     <>
-      <h1>Current emotion is {emotion}</h1>
-      <button onClick={() => setEmotion("happy")}>Happy</button>
+      <h1>
+        Current emotion is {emotion} and {secondary}
+      </h1>
+      <button onClick={() => setEmotion("happy")}>Make Happy</button>
       <button onClick={() => setEmotion("frustrated")}>Frustrate</button>
       <button onClick={() => setEmotion("enthusiastic")}>Enthuse</button>
     </>
