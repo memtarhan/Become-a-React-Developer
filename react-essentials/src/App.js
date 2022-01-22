5f2ad4a4b6c34ea44ee08862151f8be8557d2e9e
Mehmet Tarhan <hi@memtarhan.com>
2022-01-22 00:16:39 +0000

    [React Essentials] - Nesting links with React Router 6

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -1,13 +1,26 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import { Home, About, Events, Contact, Whoops404 } from "./pages";
+import {
+  Home,
+  About,
+  Events,
+  Contact,
+  Whoops404,
+  CompanyServices,
+  CompanyHistory,
+  CompanyLocation,
+} from "./pages";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/about" element={<About />}>
+          <Route path="services" element={<CompanyServices />} />
+          <Route path="history" element={<CompanyHistory />} />
+          <Route path="location" element={<CompanyLocation />} />
+        </Route>
         <Route path="/events" element={<Events />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<Whoops404 />} />
--- react-essentials/src/pages.js
+++ react-essentials/src/pages.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, Outlet } from "react-router-dom";
 
 export function Home() {
   return (
@@ -18,6 +18,31 @@ export function About() {
   return (
     <div>
       <h1>[About]</h1>
+      <Outlet />
+    </div>
+  );
+}
+
+export function CompanyServices() {
+  return (
+    <div>
+      <h2>Our Services</h2>
+    </div>
+  );
+}
+
+export function CompanyHistory() {
+  return (
+    <div>
+      <h2>Our History</h2>
+    </div>
+  );
+}
+
+export function CompanyLocation() {
+  return (
+    <div>
+      <h2>Our Location</h2>
     </div>
   );
 }
