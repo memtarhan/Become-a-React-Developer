651e65326d824fc374aed8fd1b946e49da013fba
Mehmet Tarhan <hi@memtarhan.com>
2022-01-22 17:03:36 +0000

    [Building an Interface] - Setup project with tailwind css

---
+++ react-interface/src/App.js
@@ -0,0 +1,13 @@
+import { BiArchive } from "react-icons/bi";
+
+function App() {
+  return (
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-3xl">
+        <BiArchive className="inline-block text-red-400 align-center" />
+        Your Appointments
+      </h1>
+    </div>
+  );
+}
+export default App;
