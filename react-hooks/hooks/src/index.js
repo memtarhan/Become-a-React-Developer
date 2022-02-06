eff1a6df60a12f6298811424da71f89b06ed4ad9
Mehmet Tarhan <hi@memtarhan.com>
2022-02-06 19:04:22 +0000

    [React Hooks] - Working with component trees

--- react-hooks/hooks/src/index.js
+++ react-hooks/hooks/src/index.js
@@ -1,19 +1,20 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import { FaStar } from "react-icons/fa";
+
+const createArray = (length) => [...Array(length)];
+
+function Star({ selected = false }) {
+  return <FaStar color={selected ? "red" : "gray"} />;
+}
+
+function StarRating({ totalStars = 5 }) {
+  return createArray(totalStars).map((n, i) => <Star key={i} />);
+}
 
 function App() {
-  const [checked, setChecked] = useState(false);
-  return (
-    <div>
-      <input
-        type="checkbox"
-        value={checked}
-        onChange={() => setChecked(!checked)}
-      />
-      <p>{checked ? "checked" : "not checked"} </p>
-    </div>
-  );
+  return <StarRating totalStars={10} />;
 }
 
 ReactDOM.render(
