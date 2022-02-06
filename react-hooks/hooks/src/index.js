e21d12d95fbdf35cda8e64eee0904277c88ebb01
Mehmet Tarhan <hi@memtarhan.com>
2022-02-06 01:39:02 +0000

    [React Hooks] - Reviewing Array and Object destructuring

--- react-hooks/hooks/src/index.js
+++ react-hooks/hooks/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from "./App";
+
+const people = ["Alex", "Ali", "Anna"];
+
+// - Array destructuring
+const [first, second] = ["Alex", "Ali", "Anna"];
+console.log(first, second); // prints out: Alex Ali
+
+const [, , onlyThird] = ["Alex", "Ali", "Anna"];
+console.log(onlyThird); // prints out: Anna
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App name="Mehmet" />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
