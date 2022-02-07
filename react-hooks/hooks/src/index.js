508a120b2ba56319d9e778c88bb87085813ca7df
Mehmet Tarhan <hi@memtarhan.com>
2022-02-07 00:31:58 +0000

    [React Hooks] - Working with the dependency array

--- react-hooks/hooks/src/index.js
+++ react-hooks/hooks/src/index.js
@@ -4,14 +4,21 @@ import "./index.css";
 
 function App() {
   const [name, setName] = useState("Mehmet");
+  const [admin, setAdmin] = useState(false);
   useEffect(() => {
+    console.log(`Celebrate ${name}`);
     document.title = `Celebrate ${name}`;
-  });
+  }, [name]);
 
+  useEffect(() => {
+    console.log(`The user is ${admin ? "admin" : "not admin"}`);
+  }, [admin]);
   return (
     <section>
       <p>Congrats {name}!</p>
       <button onClick={() => setName("Somebody")}>Change Winner</button>
+      <p>{admin ? "logged in" : "not logged in"}</p>
+      <button onClick={() => setAdmin(true)}>Log in</button>
     </section>
   );
 }
