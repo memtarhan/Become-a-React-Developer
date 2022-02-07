4a52b7cb18fe864290705bd12a535893bec1f4b9
Mehmet Tarhan <hi@memtarhan.com>
2022-02-07 00:45:24 +0000

    [React Hooks] - Fetching data with useEffect

--- react-hooks/hooks/src/index.js
+++ react-hooks/hooks/src/index.js
@@ -3,24 +3,27 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 function App() {
-  const [name, setName] = useState("Mehmet");
-  const [admin, setAdmin] = useState(false);
-  useEffect(() => {
-    console.log(`Celebrate ${name}`);
-    document.title = `Celebrate ${name}`;
-  }, [name]);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    console.log(`The user is ${admin ? "admin" : "not admin"}`);
-  }, [admin]);
-  return (
-    <section>
-      <p>Congrats {name}!</p>
-      <button onClick={() => setName("Somebody")}>Change Winner</button>
-      <p>{admin ? "logged in" : "not logged in"}</p>
-      <button onClick={() => setAdmin(true)}>Log in</button>
-    </section>
-  );
+    fetch(`https://api.github.com/users`)
+      .then((response) => response.json())
+      .then(setData);
+  }, []);
+
+  if (data) {
+    return (
+      <div>
+        <ul>
+          {data.map((user) => {
+            <li>{user.login}</li>;
+          })}
+        </ul>
+        <button onClick={() => setData([])}>Remove Data</button>
+      </div>
+    );
+  }
+  return <p>No users</p>;
 }
 
 ReactDOM.render(
